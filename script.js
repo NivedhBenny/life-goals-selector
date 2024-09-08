827c13110d182a86df3f35f3ea9c581b6ddc4ce1
Nivedh Benny <nivedhbenny2001@gmail.com>
2024-09-08 16:21:21 +0530

    for more players

--- script.js
+++ script.js
@@ -8,30 +8,42 @@ const goals = [
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
+let goalTypes=[];
+let goalNumbers=[];
 
-
-
-
-
-function changeImages() {
-    let firstGoalType = getRandomInt(4);
-    let firstGoalNumber = getRandomInt(2);
-    let firstGoal = goals[firstGoalType][firstGoalNumber];
-    
-    let secondGoalType = getRandomInt(4);
-    while (secondGoalType == firstGoalType) {
-      secondGoalType = getRandomInt(4);
+function getGoals(players){
+  for(let i=1;i<=players;i++){
+    let type=getRandomInt(4);
+    while(goalTypes.includes(type)){
+      type=getRandomInt(4);
     }
-    
-    let secondGoalNumber = getRandomInt(2);
-    
-    let secondGoal = goals[secondGoalType][secondGoalNumber];
-    document.getElementById('card1').querySelector('img').src = `images/card${firstGoalType}${firstGoalNumber}.jpg`;
-    document.getElementById('card2').querySelector('img').src = `images/card${secondGoalType}${secondGoalNumber}.jpg`;
+    let index=getRandomInt(2);
+    goalTypes.push(type);
+    goalNumbers.push(index);
+  }
+}
+function getAllGoals(){
+  for(let i=1;i<=4;i++){
+    goalTypes.push(i-1);
+    goalNumbers.push(getRandomInt(2));
+  }
+}
+function changeGoals() {
+    let players = parseInt(document.getElementById('changeGoals').getAttribute('players'));
+    console.log(players);
+    if(players===4){
+      getAllGoals();
+    }else{
+      getGoals(players);
+    }
+    for(let i=0;i<goalTypes.length;i++){
+      document.getElementById(`card${i+1}`).querySelector('img').src = `images/card${goalTypes[i]}${goalNumbers[i]}.jpg`;
+    }
+    // document.getElementById('card1').querySelector('img').src = `images/card${firstGoalType}${firstGoalNumber}.jpg`;
+    // document.getElementById('card2').querySelector('img').src = `images/card${secondGoalType}${secondGoalNumber}.jpg`;
+    document.getElementById('changeGoals').innerText="Change Goals";
+    goalTypes=[];
+    goalNumbers=[];
 }
 
-document.getElementById('changeGoals').addEventListener('click', changeImages);
-
-
-console.log(firstGoal);
-console.log(secondGoal);
+document.getElementById('changeGoals').addEventListener('click', changeGoals);
