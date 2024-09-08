8d708571ae973dbb18ccbb9425c4660055e0c13a
Nivedh Benny <115915588+NivedhBenny@users.noreply.github.com>
2024-09-08 13:03:10 +0530

    removed unnecessary variables
    like firstGoal and SecondGoal.

--- script.js
+++ script.js
@@ -9,29 +9,17 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-
-
-
-
 function changeImages() {
     let firstGoalType = getRandomInt(4);
     let firstGoalNumber = getRandomInt(2);
-    let firstGoal = goals[firstGoalType][firstGoalNumber];
-    
     let secondGoalType = getRandomInt(4);
     while (secondGoalType == firstGoalType) {
       secondGoalType = getRandomInt(4);
     }
-    
     let secondGoalNumber = getRandomInt(2);
-    
-    let secondGoal = goals[secondGoalType][secondGoalNumber];
+  
     document.getElementById('card1').querySelector('img').src = `images/card${firstGoalType}${firstGoalNumber}.jpg`;
     document.getElementById('card2').querySelector('img').src = `images/card${secondGoalType}${secondGoalNumber}.jpg`;
 }
 
 document.getElementById('changeGoals').addEventListener('click', changeImages);
-
-
-console.log(firstGoal);
-console.log(secondGoal);
